14e01c108868d649a16fbeaad5ba684d4df9b62d
datamaning <george_xl@163.com>
2017-02-12 11:59:07 +0800

    svm kernel rbf

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -25,10 +25,12 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
-features_train=features_train[:len(features_train)/100]
-labels_train=labels_train[:len(labels_train)/100]
+#features_train=features_train[0:len(features_train)/100]
+#labels_train=labels_train[0:len(labels_train)/100]
+print len(features_train),len(labels_train)
+
 from sklearn import svm
-clf=svm.SVC(kernel='linear')
+clf=svm.SVC(kernel='rbf',C=10000.0)
 
 t1=time()
 clf.fit(features_train,labels_train)
@@ -36,10 +38,21 @@ print 'train time consuming',round(time()-t1,3),'s'
 
 t0=time()
 pred=clf.predict(features_test)
+num0=0
+num1=0
+for i in range(len(pred)):
+    if pred[i]==0:
+        num0+=1
+    elif pred[i]==1:
+        num1+=1
+print num0,num1
 print "time consuming",round(time()-t0,3),'s'
+
 
 from sklearn.metrics import accuracy_score
 score=accuracy_score(pred,labels_test)
+score=clf.score(features_test,labels_test)
+
 print "Answer is ",score
 #########################################################
 
