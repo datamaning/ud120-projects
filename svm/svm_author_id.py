a6f887f6932158967281b53f3518615695c78160
datamaning <george_xl@163.com>
2017-02-12 10:53:50 +0800

    svm score

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -24,7 +24,14 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn import svm
+clf=svm.SVC()
+clf.fit(features_train,labels_train)
 
+pred=clf.predict(features_test)
+from sklearn.metrics import accuracy_score
+score=accuracy_score(pred,labels_test)
+print "Answer is ",score
 #########################################################
 
 
